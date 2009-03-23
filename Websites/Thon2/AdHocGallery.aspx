0ea624d5b22fa8fd778af5cddf0065c0d0088622
Home <Home@52838b7f-0bfc-d347-9dde-de7af8c8e93b>
2009-03-23 08:29:23 +0000

    Ad Hoc Gallery
    git-svn-id: svn://localhost@71 52838b7f-0bfc-d347-9dde-de7af8c8e93b

---
+++ Websites/Thon2/AdHocGallery.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ThonMasterPage.master" AutoEventWireup="true" CodeFile="AdHocGallery.aspx.cs" Inherits="Thon.AdHocGallery" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphhead" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphmain" Runat="Server">
+    <div style="margin-left:20px;">
+    Actual size images matching ' <%=Request.QueryString["folder"] %> '<br />    
+    </div>
+    <asp:Table ID="Table1" CellPadding="30" CellSpacing="10" runat="server">
+    </asp:Table>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="cphsidebar" Runat="Server">
+</asp:Content>
+
---
+++ Websites/Thon2/AdHocGallery.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Thon.Support.Web.Controls;
+using System.Web.UI.HtmlControls;
+using System.IO;
+
+namespace Thon
+{
+    public partial class AdHocGallery : ThonBasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string which = Request.QueryString["folder"];
+
+            if (which == null) Response.Redirect("Errors/404.aspx");
+            if (which == string.Empty) Response.Redirect("Errors/404.aspx");
+            string[] imges = { "" };
+
+            try
+            {
+                imges = Directory.GetFiles(Server.MapPath("~/Images/AdHocGallery/" + which));
+            }
+            catch
+            {
+                Response.Redirect("Errors/404.aspx");
+            }
+
+            if ((imges.Length == 1) && imges[0] == "") Response.Redirect("Errors/404.aspx");
+
+            foreach (string item in imges)
+            {
+                string actname = item.Substring(item.LastIndexOf('\\') + 1);
+                actname = "Images/AdHocGallery/" + which + "/" + actname;
+                TableCell tc = new TableCell();
+                TableRow tr = new TableRow();
+                HtmlGenericControl c = new HtmlGenericControl("c");
+                HtmlGenericControl img = new HtmlGenericControl("img");
+                img.Attributes.Add("src", actname);
+                img.Attributes.Add("height", "100%");
+                img.Attributes.Add("width", "100%");
+                c.Controls.Add(img);
+                tc.Controls.Add(c);
+                tr.Cells.Add(tc);
+                Table1.Rows.Add(tr);
+            }
+        }
+    }
+}
